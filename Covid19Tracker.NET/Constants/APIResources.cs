878c5d201edd4c0e43051e0704708232fa3b27a3
egbakou <egbakoukodjolaurent@gmail.com>
2020-05-14 22:45:18 +0000

    @ExpDev07 data removed from the project.

--- Covid19Tracker.NET/Constants/APIResources.cs
+++ Covid19Tracker.NET/Constants/APIResources.cs
@@ -2,12 +2,7 @@
 {
     public class APIResources
     {
-        public static string API_BASE_URI = "https://coronavirus-tracker-api.herokuapp.com/v2/";
-        public static string SOURCES_ENDPOINT = "sources";
-        public static string LATEST_ENDPOINT = "latest";
-        public static string LOCATIONS_ENDPOINT = "locations";
-        public static string COUNTRY_QUERY_PARAMS = "locations?country=";
-        public static string COUNTRYCODE_QUERY_PARAMS = "locations?country_code=";
+        public static string API_BASE_URI = "";
 
 
     }
--- Covid19Tracker.NET/Services/Covid19TrackerAPI.cs
+++ Covid19Tracker.NET/Services/Covid19TrackerAPI.cs
@@ -12,7 +12,7 @@ using static Covid19Tracker.Constants.APIResources;
 namespace Covid19Tracker.Services
 {
     /// <summary>
-    /// A Class consumes an API provided by @ExpDev07(Github) for tracking the global coronavirus (COVID-19, SARS-CoV-2) outbreak.
+    /// A Class consumes an API provided by @NovelCOVID (Github) for tracking the global coronavirus (COVID-19, SARS-CoV-2) outbreak.
     /// </summary>
     public class Covid19TrackerAPI
     {
@@ -21,155 +21,8 @@ namespace Covid19Tracker.Services
         /// </summary>
         private static readonly RestClient client = new RestClient(API_BASE_URI);
 
-        /// <summary>
-        /// Get latest about total confirmed cases and deaths in the world.
-        /// </summary>
-        /// <returns>Total confirmed cases and deaths in the world.</returns>
-        public static async Task<Latest> GetLatestAsync()
-        {
-            var request = new RestRequest(
-                $"{LATEST_ENDPOINT}",
-                Method.GET,
-                DataFormat.Json);
-            IRestResponse response = await client.ExecuteGetAsync(request).ConfigureAwait(false);
-            if (response.IsSuccessful && response.StatusCode.HasFlag(HttpStatusCode.OK))
-            {
-                JObject jsonObject = (JObject)JsonConvert.DeserializeObject(response.Content);
-                var latestNode = jsonObject["latest"];
-                return latestNode.ToObject<Latest>();
-            }
-            throw new Exception("No data found. Please check if https://coronavirus-tracker-api.herokuapp.com/v2/locations is avialable.");
-        }
-
-        /// <summary>
-        /// Get latest related to a specific country based on the country code.
-        /// </summary>
-        /// <param name="countryCode">The country code.</param>
-        /// <returns>Total confirmed cases and deaths in the country.</returns>
-        public static async Task<Latest> GetLatestAboutCountryByCodeAsync(string countryCode)
-        {
-            var request = new RestRequest(
-                $"{COUNTRYCODE_QUERY_PARAMS}{countryCode.ToUpper()}",
-                Method.GET,
-                DataFormat.Json);
-            IRestResponse response = await client.ExecuteGetAsync(request).ConfigureAwait(false);
-            if (response.IsSuccessful && response.StatusCode.HasFlag(HttpStatusCode.OK))
-            {
-                JObject jsonObject = JObject.Parse(response.Content);
-                CoronavirusOutbreakData data = jsonObject.ToObject<CoronavirusOutbreakData>();
-                if (data.Locations.Count > 0)
-                    return data.Latest;
-            }
-            throw new Exception("No data found. Please, provide the correct country code !");
-        }
-
-        /// <summary>
-        /// Gets the Coordinates(Longitude and Latitude), the latest data about the
-        /// country and the last updated date based on the country code.
-        /// </summary>
-        /// <param name="countryCode">The country code.</param>
-        /// <returns>The Location of the country, his latest covid-19 data and the last updated date.</returns>
-        public static async Task<Location> GetLocationWithDataByCodeAsync(string countryCode)
-        {
-            var request = new RestRequest(
-                $"{COUNTRYCODE_QUERY_PARAMS}{countryCode.ToUpper()}",
-                Method.GET,
-                DataFormat.Json);
-            IRestResponse response = await client.ExecuteGetAsync(request).ConfigureAwait(false);
-            if (response.IsSuccessful && response.StatusCode.HasFlag(HttpStatusCode.OK))
-            {
-                JObject jsonObject = JObject.Parse(response.Content);
-                CoronavirusOutbreakData data = jsonObject.ToObject<CoronavirusOutbreakData>();
-
-                if (data.Locations.Count > 0)
-                    return data.Locations.Last();
-            }
-            throw new Exception("No data found. Please, provide the correct country code !");
-        }
-
-        /// <summary>
-        /// Get latest related to a specific country based on the country name.
-        /// </summary>
-        /// <param name="countryName">The country name.</param>
-        /// <returns>Total confirmed cases and deaths in the country.</returns>
-        public static async Task<Latest> GetLatestAboutCountryByNameAsync(string countryName)
-        {
-            var request = new RestRequest(
-                $"{LOCATIONS_ENDPOINT}",
-                Method.GET,
-                DataFormat.Json);
-            IRestResponse response = await client.ExecuteGetAsync(request).ConfigureAwait(false);
-            if (response.IsSuccessful && response.StatusCode.HasFlag(HttpStatusCode.OK))
-            {
-                JObject jsonObject = JObject.Parse(response.Content);
-                CoronavirusOutbreakData data = jsonObject.ToObject<CoronavirusOutbreakData>();
-
-                //Get the location object using the country code
-                var location = data.Locations.Where(l => l.Country.ToLower().Equals(countryName.ToLower()) && l.Province == "")
-                    .FirstOrDefault();
-                if (location != null)
-                    return location.Latest;
-            }
-            throw new Exception("No data found. Please, provide the correct country name in English language !");
-        }
-
-        /// <summary>
-        /// Gets the Coordinates(Longitude and Latitude), the latest data about the
-        /// country and the last updated date based on the country name.
-        /// </summary>
-        /// <param name="countryName">The country name.</param>
-        /// <returns>The Location of the country, his latest covid-19 data and the last updated date.</returns>
-        public static async Task<Location> GetLocationWithDataByNameAsync(string countryName)
-        {
-            var request = new RestRequest(
-                $"{LOCATIONS_ENDPOINT}",
-                Method.GET,
-                DataFormat.Json);
-            IRestResponse response = await client.ExecuteGetAsync(request).ConfigureAwait(false);
-            if (response.IsSuccessful && response.StatusCode.HasFlag(HttpStatusCode.OK))
-            {
-                JObject jsonObject = JObject.Parse(response.Content);
-                CoronavirusOutbreakData data = jsonObject.ToObject<CoronavirusOutbreakData>();
-
-                //Get the location object using the country name
-                var location = data.Locations.Where(l => l.Country.ToLower().Equals(countryName.ToLower()) && l.Province == "")
-                    .FirstOrDefault();
-                if (location != null)
-                    return location;
-            }
-            throw new Exception("No data found. Please, provide the correct country name in English language !");
-        }
-
-
-        /// <summary>
-        /// Get Data from of all locations in the world (Latest + Locations).
-        /// </summary>
-        /// <returns>Date from all location.</returns>
-        public static async Task<CoronavirusOutbreakData> GetTheWorldCovid19Data()
-        {
-            var request = new RestRequest(
-                $"{LOCATIONS_ENDPOINT}",
-                Method.GET,
-                DataFormat.Json);
-            IRestResponse response = await client.ExecuteGetAsync(request).ConfigureAwait(false);
-            if (response.IsSuccessful && response.StatusCode.HasFlag(HttpStatusCode.OK))
-            {
-                JObject jsonObject = JObject.Parse(response.Content);
-                return jsonObject.ToObject<CoronavirusOutbreakData>();
-            }
-            throw new Exception("No data found. Please check if https://coronavirus-tracker-api.herokuapp.com/v2/locations is avialable.");
-        }
-
-
-        /// <summary>
-        /// Get all countries data.
-        /// </summary>
-        /// <returns>A list of Location object(Country, Latest data, ... etc).</returns>
-        public static async Task<List<Location>> GetAllCountriesDataAsync()
-        {
-            var covid19Data = await GetTheWorldCovid19Data();
-            return covid19Data.Locations.Where(l => l.Province == "").ToList();
-        }
+        
+       
 
     }
 }
