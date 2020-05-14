be4db57541221c5b5aa3d15ca191bc3516c177b4
egbakou <egbakoukodjolaurent@gmail.com>
2020-05-14 23:20:37 +0000

    Added NOVELCovid/API endpoints

--- Covid19Tracker.NET/Constants/APIResources.cs
+++ Covid19Tracker.NET/Constants/APIResources.cs
@@ -1,9 +1,24 @@
-﻿namespace Covid19Tracker.Constants
+﻿/*
+APIResources.cs
+14/05/2020 23:18:45
+Kodjo Laurent Egbakou
+*/
+
+namespace Covid19Tracker.Constants
 {
+    /// <summary>
+    /// Defines the <see cref="APIResources" />.
+    /// </summary>
     public class APIResources
     {
-        public static string API_BASE_URI = "";
+        /// <summary>
+        /// Defines CONTINENTS DATA ENDPOINT.
+        /// </summary>
+        public static string CONTINENTS_ENDPOINT = "https://disease.sh/v2/continents";
 
-
+        /// <summary>
+        /// Defines COUNTRIES DATA ENDPOINT.
+        /// </summary>
+        public static string COUNTRIES_ENDPOINT = "https://disease.sh/v2/countries";
     }
 }
