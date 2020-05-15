a18edc4283feacada4ccaa37fafeecc89f18886c
egbakou <egbakoukodjolaurent@gmail.com>
2020-05-15 12:49:51 +0000

    Added unit test project to the solution

---
+++ Covid19Tracker.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Covid19Tracker.UnitTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
