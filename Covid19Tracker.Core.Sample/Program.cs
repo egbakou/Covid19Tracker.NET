39f8b49c47e0454a6535ff50a8abfeaab66bdc38
egbakou <egbakoukodjolaurent@gmail.com>
2020-05-15 22:36:26 +0000

    Added .Net core sample

---
+++ Covid19Tracker.Core.Sample/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using static Covid19Tracker.Services.Covid19TrackerAPI;
+
+
+namespace Covid19Tracker.Core.Sample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var result1 = await GetCountriesDataAsync();
+            Console.WriteLine(result1.Count);
+            Console.ReadLine();
+
+
+        }
+    }
+}
