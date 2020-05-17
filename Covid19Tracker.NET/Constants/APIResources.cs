ef2662694fa1fc00cfa236f297d3b81862c9fc88
egbakou <egbakoukodjolaurent@gmail.com>
2020-05-17 07:07:22 +0000

    doc added to constant class

--- Covid19Tracker.NET/Constants/APIResources.cs
+++ Covid19Tracker.NET/Constants/APIResources.cs
@@ -1,16 +1,33 @@
 ﻿/*
 APIResources.cs
 14/05/2020 23:18:45
-Kodjo Laurent Egbakou
 */
 
 namespace Covid19Tracker.Constants
 {
+    /// <summary>
+    /// Defines the <see cref="APIResources" />.
+    /// </summary>
     public class APIResources
     {
+        /// <summary>
+        /// Defines the API_BASE_URI.
+        /// </summary>
         public static string API_BASE_URI = "https://disease.sh/v2/";
+
+        /// <summary>
+        /// Defines the ALL_ENDPOINT.
+        /// </summary>
         public static string ALL_ENDPOINT = "all";
+
+        /// <summary>
+        /// Defines the CONTINENTS_ENDPOINT.
+        /// </summary>
         public static string CONTINENTS_ENDPOINT = "continents";
+
+        /// <summary>
+        /// Defines the COUNTRIES_ENDPOINT.
+        /// </summary>
         public static string COUNTRIES_ENDPOINT = "countries";
     }
 }
