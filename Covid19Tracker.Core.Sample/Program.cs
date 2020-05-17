e7944c52a7ea59200f51cbdd8b99c59a026e5d1d
egbakou <egbakoukodjolaurent@gmail.com>
2020-05-17 05:08:10 +0000

    Tested GetCountriesDataAsync's method

--- Covid19Tracker.Core.Sample/Program.cs
+++ Covid19Tracker.Core.Sample/Program.cs
@@ -14,9 +14,19 @@ namespace Covid19Tracker.Core.Sample
             //Console.WriteLine(result1.Count);
             //Console.ReadLine();
 
-            var result2 = await GetDataByContinentAsync("africa");
-            Console.WriteLine(result2.Cases);
+            //var result2 = await GetDataByContinentAsync("africa");
+            //Console.WriteLine(result2.Cases);
+            //Console.ReadLine();
+
+
+            var result3 = await GetCountriesDataAsync();
+            Console.WriteLine(result3.Count);
             Console.ReadLine();
+
+
+            //var result4 = await GetDataByCountryAsync("togo");
+            //Console.WriteLine(result4.Cases);
+            //Console.ReadLine();
 
 
         }
